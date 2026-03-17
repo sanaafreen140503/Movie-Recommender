68b23e24ab4bbe523bb3802f052a467e70a27ae2
sanaafreen140503 <sanaafreen141105@gmail.com>
2026-03-17 21:15:24 +0530

    Add files via upload

--- model.py
+++ model.py
@@ -2,30 +2,26 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-# Load datasets
+# Load dataset
 movies = pd.read_csv('dataset/tmdb_5000_movies.csv')
-credits = pd.read_csv('dataset/tmdb_5000_credits.csv')
 
-# Merge datasets
-movies = movies.merge(credits, on='title')
-
-# Select columns
-movies = movies[['movie_id', 'title', 'overview']]
+# Select required columns (FIXED: id instead of movie_id)
+movies = movies[['id', 'title', 'overview']]
 
 # Remove missing values
 movies.dropna(inplace=True)
 
-# Vectorization
+# Convert text into vectors
 cv = CountVectorizer(max_features=5000, stop_words='english')
 vectors = cv.fit_transform(movies['overview']).toarray()
 
-# Similarity matrix
+# Compute similarity
 similarity = cosine_similarity(vectors)
 
 # Recommendation function
 def recommend(movie):
-    index = movies[movies['title'] == movie].index[0]
-    distances = similarity[index]
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
 
     movie_list = sorted(
         list(enumerate(distances)),
@@ -37,7 +33,7 @@ def recommend(movie):
     movie_ids = []
 
     for i in movie_list:
-        movie_ids.append(movies.iloc[i[0]].movie_id)
+        movie_ids.append(movies.iloc[i[0]].id)   # FIXED
         recommended_movies.append(movies.iloc[i[0]].title)
 
     return recommended_movies, movie_ids
