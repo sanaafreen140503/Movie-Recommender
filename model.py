4fbd2c5ad0d035a19225c4e0a391c3baffdc4094
sanaafreen140503 <sanaafreen141105@gmail.com>
2026-03-17 21:04:50 +0530

    Add files via upload

--- model.py
+++ model.py
@@ -1,26 +1,43 @@
 import pandas as pd
-import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+# Load datasets
 movies = pd.read_csv('dataset/tmdb_5000_movies.csv')
 credits = pd.read_csv('dataset/tmdb_5000_credits.csv')
 
-movies = movies.merge(credits,on='title')
+# Merge datasets
+movies = movies.merge(credits, on='title')
 
-movies = movies[['movie_id','title','overview']]
+# Select columns
+movies = movies[['movie_id', 'title', 'overview']]
 
+# Remove missing values
 movies.dropna(inplace=True)
 
-cv = CountVectorizer(max_features=5000,stop_words='english')
-
+# Vectorization
+cv = CountVectorizer(max_features=5000, stop_words='english')
 vectors = cv.fit_transform(movies['overview']).toarray()
 
+# Similarity matrix
 similarity = cosine_similarity(vectors)
-#similarity = cosine_similarity(vectors)
 
-pickle.dump(movies,open('movies.pkl','wb'))
+# Recommendation function
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = similarity[index]
 
-from model import similarity
+    movie_list = sorted(
+        list(enumerate(distances)),
+        reverse=True,
+        key=lambda x: x[1]
+    )[1:6]
 
-#pickle.dump(similarity,open('similarity.pkl','wb'))
+    recommended_movies = []
+    movie_ids = []
+
+    for i in movie_list:
+        movie_ids.append(movies.iloc[i[0]].movie_id)
+        recommended_movies.append(movies.iloc[i[0]].title)
+
+    return recommended_movies, movie_ids
